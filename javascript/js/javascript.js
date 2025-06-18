4576a5c38255025451cf9fdb85b3f1689860e6d3
Rakesh-Codder <rakeshpandit5556@gmail.com>
2025-06-19 05:14:06 +0530

    Add files via upload

---
+++ javascript/js/javascript.js
@@ -0,0 +1,177 @@
+// // console.log("rakesh");
+// let number = [85,97,44,37,76,60];
+// let sum=0;
+// for(let i=0;i<number.length;i++){
+//     sum=sum+number[i];
+// }
+// let average=sum/number.length;
+// console.log(average);
+
+
+// let item=[250,645,300,900,50];
+// for(let i=0;i<item.length;i++){
+//     let offer=item[i]/10;
+//     item[i]-=offer;
+// }
+// console.log(item);
+
+//  let companies=["Bloomberg","Microsoft","Uber","googal","IBM","netflix"];
+//      companies.splice(2,1,'ola');
+//  console.log(rakesh);
+//   let rakesh=companies.push("Amazon");
+
+
+// function myfunction(){
+//     console.log("welocome to apna collage");
+//     console.log("we are learning js");
+// }
+
+
+// function myfunction(msg){
+//     console.log(msg)
+// }
+// myfunction("i love you");
+
+
+// function sum( a,b){
+//     console.log(a+b);
+// }
+// sum(4,9);
+
+// function sum(x,y){
+//     s=x+y;
+//     return s;
+// }
+// let value=sum(8,422);
+// console.log(value);
+
+//  const sum=(a,b)=>{
+//    return a+b;
+// }
+// console.log(sum);
+
+// const mul=(a,b)=>{
+//     console.log(a*b);
+//     return a*b;
+
+// }
+   
+
+
+// finding a vowel
+
+
+
+
+// function countvowels(str){
+//     for(let i=0; i<str.length;i++){
+//        if(str[i]==="a"||str[i]==="e"||str[i]==="o"||str[i]==="i"||str[i]==="u" ) {
+//         console.log(str[i]);
+//        }
+//     }
+
+// }
+
+
+
+
+// let count=(str)=>{
+
+//     for(let i=0; i<str.length;i++){
+//                if(str[i]==="a"||str[i]==="e"||str[i]==="o"||str[i]==="i"||str[i]==="u" ) {
+//                 console.log(str[i]);
+//                }
+//         }
+//     }
+//     count("rakeshkumarpandit");
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+// arr.forEach(function sum(val){
+   
+//     console.log(val);
+// })
+
+// let str=["rakesh ","amit","dheeraj","alok"];
+// str.forEach(function print(val){
+//     console.log(val)
+// })
+
+
+// let str=["rakesh ","amit","dheeraj","alok"];
+// str.forEach((val,idx,str)=>{
+//     console.log(val,idx,str)
+// })
+
+
+
+// let arr =[1,2,3,4,5,6];
+// arr.forEach(function multiply(n)
+// {mul=n*n;
+//     console.log(mul);
+// })
+
+
+
+// let arr=[1,2,3,4,5,6];
+// arr.forEach((mul)=>{
+//     multiply=mul*mul;
+//     console.log(multiply);
+// })
+
+
+
+
+// let arr=[1,2,3,4,5];
+//  const output= arr.reduce((res,curr)=>{return res+curr});
+// console.log(output);
+
+
+// let arr=[1,2,3,4,5];
+// const output=arr.reduce((pre,cur)=>{return pre >cur ?pre:cur});
+// console.log(output)
+
+
+
+//dom
+
+
+// console.dir(document);
+// console.log(document.body.childNodes);
+
+// document.body.style.background="pink";
+// //selecting with id 
+// let document1=document.getElementById("heading");
+// console.dir(document);
+// console.log(document1);
+// //selecting with class
+// let document2= document.getElementsByClassName("green");
+// console.dir(document2);
+// console.log(document2);
+// //selecting with tag
+// let document3=document.getElementsByTagName("p");
+// console.dir(document3);
+//querey selector  // using=class
+// let document4=document.querySelector(".green");
+// console.dir(document4);
+// console.log(document4);
+// //all   //using=id
+// let document5=document.querySelectorAll("#heading");
+// console.dir(document5);
+// console.log(document5);
+//using tag
+// let document6=document.querySelector("#myid");
+// console.dir(document6);
+// console.log(document6);
+
+
+//dom properties
+//(1)
+// let firstEl = document.querySelector("#myid");
+// console.dir(firstEl);
+//(2)
+let div=document.querySelector("div")
+console.dir(div)
+
+
+
+
---
+++ javascript/js/notes.js
@@ -0,0 +1,14 @@
+// let n = prompt("enter a number");
+// console.log(n);  
+// for(let i=0;i<=n;i++){
+//     console.log(i);  
+// }
+
+
+let h2=document.querySelector("h2");
+console.dir(h2);
+
+
+h2.innerText=h2.innerText+" rakesh kumar";
+console.dir(h2);
+
